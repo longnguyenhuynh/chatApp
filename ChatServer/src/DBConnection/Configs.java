1209d4c9e65fb0814103c1826490e1b77da01c50
Nguyen Huynh Long <long.nguyenhuynh611@gmail.com>
2020-05-26 16:43:13 +0700

    loi concurrent

--- ChatServer/src/DBConnection/Configs.java
+++
@@ -1,9 +0,0 @@
-package DBConnection;
-
-public class Configs {
-    public static String dbhost = "localhost";
-    public static String dbport = "3306";
-    public static String dbuser = "root";
-    public static String dbpass = "h0icaigi";
-    public static String dbname = "chat";
-}

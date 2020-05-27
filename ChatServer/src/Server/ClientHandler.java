1c328cb24696c37a8908782df806f948c61af39d
Nguyen Huynh Long <long.nguyenhuynh611@gmail.com>
2020-05-27 15:08:21 +0700

    test

---
+++ ChatServer/src/Server/ClientHandler.java
@@ -0,0 +1,89 @@
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Iterator;
+
+// ClientHandler class
+class ClientHandler implements Runnable {
+    public String name;
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    Socket s;
+
+    // constructor
+    public
+    ClientHandler(Socket s, String name, DataInputStream dis, DataOutputStream dos) {
+        this.name = name;
+        this.dis = dis;
+        this.dos = dos;
+        this.s = s;
+    }
+
+    @Override
+    public
+    void run() {
+        String received;
+        while (true) {
+            try {
+                // receive the string
+                received = dis.readUTF();
+
+                // break the string into message and recipient part
+                String[] msgSplit     = received.split("#", 2);
+
+                for (ClientHandler clientHandler : Server.clientHandlerVector) {
+                    if (clientHandler.name.equals(msgSplit[0])) {
+                        clientHandler.dos.writeUTF(this.name + "#" + msgSplit[1]);
+                        break;
+                    }
+                }
+
+            } catch (IOException e) {
+                try {
+                    Iterator<ClientHandler> iterator = Server.clientHandlerVector.iterator();
+                    while (iterator.hasNext()) {
+                        ClientHandler clientHandler = iterator.next();
+                        if (clientHandler.equals(this)) {
+                            RemoveClient(clientHandler.name);
+                            iterator.remove();
+                        }
+                    }
+                    this.s.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+
+        }
+    }
+    void RemoveClient(String name) throws IOException {
+        Iterator<ClientHandler> iterator = Server.clientHandlerVector.iterator(); // java.util.ConcurrentModificationException
+        while (iterator.hasNext()) {
+            ClientHandler clientHandler = iterator.next();
+            clientHandler.dos.writeUTF("REMOVE_USER#" + name);
+        }
+    }
+
+    public
+    void AddOnlineClient(String name) throws IOException {
+        this.dos.writeUTF("NEW_USER#" + name);
+    }
+
+    public
+    void AddAllOnlineClient() throws IOException {
+        StringBuilder msg = new StringBuilder("ALL_USER#");
+
+        Iterator<ClientHandler> iterator = Server.clientHandlerVector.iterator(); // java.util.ConcurrentModificationException
+        while (iterator.hasNext()) {
+            ClientHandler clientHandler = iterator.next();
+            msg.append(clientHandler.name).append("#");
+        }
+
+        if(!msg.toString().equals("ALL_USER#")) { //first user
+            this.dos.writeUTF(msg.toString());
+        }
+    }
+}
--- ChatServer/src/Server/Server.java
+++ ChatServer/src/Server/Server.java
@@ -7,8 +7,7 @@ import java.net.*;
 public
 class Server {
 
-    // Vector to store active clients
-    static Vector<ClientHandler> ar = new Vector<>();
+    static Vector<ClientHandler> clientHandlerVector = new Vector<>();
 
     public static
     void main(String[] args) throws IOException {
@@ -27,14 +26,14 @@ class Server {
             // Create a new handler object for handling this request.
             ClientHandler client = new ClientHandler(s, dis.readUTF(), dis, dos);
 
-            for (ClientHandler clt : Server.ar) { //notify current users new user
-                clt.AddOnlineClient(client.name);
+            for (ClientHandler clientHandler : clientHandlerVector) {
+                clientHandler.AddOnlineClient(client.name);
             }
 
             client.AddAllOnlineClient();
 
             // add new client to active clients list
-            ar.add(client);
+            clientHandlerVector.add(client);
 
             // Create a new Thread with this object.
             Thread t = new Thread(client);
@@ -45,84 +44,3 @@ class Server {
     }
 }
 
-// ClientHandler class
-class ClientHandler implements Runnable {
-    public String name;
-    final DataInputStream dis;
-    final DataOutputStream dos;
-    Socket s;
-
-    // constructor
-    public
-    ClientHandler(Socket s, String name, DataInputStream dis, DataOutputStream dos) {
-        this.dis = dis;
-        this.dos = dos;
-        this.name = name;
-        this.s = s;
-    }
-
-    @Override
-    public
-    void run() {
-        String received;
-        while (true) {
-            try {
-                // receive the string
-                received = dis.readUTF();
-
-                // break the string into message and recipient part
-                String[] msgSplit     = received.split("#", 2);
-
-                // search for the recipient in the connected devices list.
-                // ar is the vector storing client of active users
-                for (ClientHandler mc : Server.ar)
-                {
-                    // if the recipient is found, write on its
-                    // output stream
-                    if (mc.name.equals(msgSplit[0]))
-                    {
-                        mc.dos.writeUTF(this.name + "#" + msgSplit[1]);
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                try { //
-                    Iterator<ClientHandler> iterator = Server.ar.iterator(); // java.util.ConcurrentModificationException
-                    while (iterator.hasNext()) {
-                        ClientHandler clientHandler = iterator.next();
-                        if (clientHandler.equals(this)) {
-                            RemoveClient(clientHandler.name);
-                            iterator.remove();
-                        }
-                    }
-                    this.s.close();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-            }
-
-        }
-    }
-
-    void RemoveClient(String name) throws IOException {
-        for (ClientHandler clt : Server.ar) { //notify current users new user
-            clt.dos.writeUTF("REMOVE_USER#" + name);
-        }
-    }
-
-    public
-    void AddOnlineClient(String name) throws IOException {
-        this.dos.writeUTF("NEW_USER#" + name);
-    }
-
-    public
-    void AddAllOnlineClient() throws IOException {
-        StringBuilder msg = new StringBuilder("ALL_USER#");
-        for (ClientHandler clt : Server.ar) {
-            msg.append(clt.name).append("#");
-        }
-        if(!msg.toString().equals("ALL_USER#")) { //first user
-            this.dos.writeUTF(msg.toString());
-        }
-    }
-}
